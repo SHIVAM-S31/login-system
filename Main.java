2f5ea650d451bccaa64944b699d3b672a6623bc0
SHIVAM-S31 <112200845+SHIVAM-S31@users.noreply.github.com>
2022-08-27 00:42:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,134 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+  //package com.demo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+//import marksSystem.StudentMarks;
+
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+//import marksSystem.StudentMarks;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public void  Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("USER LOGIN ");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 35));
+		lblNewLabel.setBounds(103, 25, 226, 24);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("User Name");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(42, 90, 78, 32);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblNewLabel_1_1.setBounds(42, 133, 78, 32);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField = new JTextField();
+		textField.setBounds(144, 97, 239, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(144, 140, 239, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JButton btnNewButton = new JButton("LOGIN");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = textField.getText();
+				String password = passwordField.getText();
+				if(password.contains("world")&& username.contains("hello")) {
+					//StudentMarks.main(null);
+					textField.setText(null);
+					textField.setText(null);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "       INVALID LOGIN      ","ERROR",JOptionPane.ERROR_MESSAGE);
+					}
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		btnNewButton.setBounds(59, 211, 88, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnReset = new JButton("RESET");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(null);	
+				passwordField.setText(null);}
+		});
+		btnReset.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		btnReset.setBounds(198, 211, 88, 23);
+		frame.getContentPane().add(btnReset);
+		
+		JButton btnExit = new JButton("EXIT");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			  }
+		});
+		btnExit.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		btnExit.setBounds(319, 211, 88, 23);
+		frame.getContentPane().add(btnExit);
+	}
+}
+
